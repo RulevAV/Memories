912a47b9df150f4c4f66d8b476e995aa928e9d05
RulevAV <rulandrei99@mail.ru>
2025-04-15 22:47:28 +0700

    add router

--- memories.client/src/app/app-routing.module.ts
+++ memories.client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './Pages/home/home.component';
+import {PostComponent} from './Pages/post/post.component';
+import {NotFoundComponent} from './Pages/not-found/not-found.component';
+import {ProfileComponent} from './Pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: HomeComponent},
+  { path: "post", component: PostComponent},
+  { path: "profile", component: ProfileComponent},
+  { path: "**", component: NotFoundComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- memories.client/src/app/app.module.ts
+++ memories.client/src/app/app.module.ts
@@ -4,14 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { ProfileComponent } from './Pages/profile/profile.component';
+import { PostComponent } from './Pages/post/post.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    PostComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
