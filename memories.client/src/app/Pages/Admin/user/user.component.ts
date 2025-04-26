f4987cff14f1f27ff31ca77e4e5c833ea7473f05
RulevAV <rulandrei99@mail.ru>
2025-04-26 07:06:04 +0700

    save

--- memories.client/src/app/Pages/Admin/user/user.component.ts
+++ memories.client/src/app/Pages/Admin/user/user.component.ts
@@ -1,15 +1,22 @@
-import {AfterViewInit, Component, inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject, ViewChild} from '@angular/core';
 import {PageEvent} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {UserService} from '../../../services/user.service';
 import User from '../../../../model/user';
 import Role from '../../../../model/role';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-
-interface Item {
-  id: number;
-  name: string;
-}
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  merge,
+  Observable,
+  OperatorFunction,
+  Subject
+} from 'rxjs';
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user',
@@ -62,10 +69,90 @@ async  handlePageEvent(e: PageEvent) {
    return roles.map(r=>r.name).join();
   }
 
-  searchTerm: string = '';
-  items: string[] = ['Apple', 'Banana', 'Cherry', 'Date', 'Fig', 'Grape', 'Honeydew'];
+  model: any;
 
-  get filteredItems() {
-    return this.items.filter(item => item.toLowerCase().includes(this.searchTerm.toLowerCase()));
+  // @ts-ignore
+  @ViewChild('instance', { static: true }) instance: NgbTypeahead;
+
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map((term) =>
+        (term === '' ? states : states.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10),
+      ),
+    );
+  };
+  onItemSelected(event: any) {
+    const selectedItem = event.item; // Usually `item` contains the selected item
+    console.log('Selected Item:', event);
+    // Update your model or perform other actions
   }
 }
+
+const states = [
+  'Alabama',
+  'Alaska',
+  'American Samoa',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+  'Delaware',
+  'District Of Columbia',
+  'Federated States Of Micronesia',
+  'Florida',
+  'Georgia',
+  'Guam',
+  'Hawaii',
+  'Idaho',
+  'Illinois',
+  'Indiana',
+  'Iowa',
+  'Kansas',
+  'Kentucky',
+  'Louisiana',
+  'Maine',
+  'Marshall Islands',
+  'Maryland',
+  'Massachusetts',
+  'Michigan',
+  'Minnesota',
+  'Mississippi',
+  'Missouri',
+  'Montana',
+  'Nebraska',
+  'Nevada',
+  'New Hampshire',
+  'New Jersey',
+  'New Mexico',
+  'New York',
+  'North Carolina',
+  'North Dakota',
+  'Northern Mariana Islands',
+  'Ohio',
+  'Oklahoma',
+  'Oregon',
+  'Palau',
+  'Pennsylvania',
+  'Puerto Rico',
+  'Rhode Island',
+  'South Carolina',
+  'South Dakota',
+  'Tennessee',
+  'Texas',
+  'Utah',
+  'Vermont',
+  'Virgin Islands',
+  'Virginia',
+  'Washington',
+  'West Virginia',
+  'Wisconsin',
+  'Wyoming',
+];
