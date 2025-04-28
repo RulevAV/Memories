10ebebccef3680b72af1c7b72b544e34b084c371
RulevAV <rulandrei99@mail.ru>
2025-04-28 21:21:07 +0700

    fix

---
+++ Memories.Server/Controllers/CardController.cs
@@ -0,0 +1,30 @@
+﻿using Memories.Server.Entities;
+using Memories.Server.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Memories.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private ICardR _cardR;
+
+        public CardController(ILogger<UserController> logger, ICardR cardR)
+        {
+            _logger = logger;
+            _cardR = cardR;
+        }
+
+        [HttpGet("[action]")]
+        [Authorize]
+        public int InfoUser()
+        {
+            var userId = User.Claims.First(u => u.Type == "Id").Value;
+            return 1;
+            //return _userR.GetUser(Guid.Parse(userId));
+        }
+    }
+}
